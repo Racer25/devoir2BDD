f89c22586d1d5d80faef0097fc3fc1e958107e66
Charles Cousyn <char-cou@hotmail.fr>
2016-11-25 16:39:17 -0500

    initial commit

---
+++ serveur2.js
@@ -0,0 +1,94 @@
+var MongoClient = require('mongodb').MongoClient,
+	test = require('assert');
+
+MongoClient.connect('mongodb://localhost:27017/dataBase', function(error, db) 
+{
+	if (error) return funcCallback(error);
+	
+    console.log("Connecté à la base de données");
+
+	// Create a test collection
+	var views=generateViews();
+	var part1 = db.collection('part1');
+	
+	part1.removeMany(); //Efface tout (sync)
+	
+	part1.insertMany(views, {w:1}).then
+	(
+		function()
+		{
+			console.log("J'ai fini d'insérer");
+			// Peform the map reduce
+			console.log("Je vais faire un mapReduce");
+			
+			var map = function ()
+			{
+				key={url:this.url,hour:this.hour};
+				emit(key, 1);
+			}
+
+			var reduce = function (key, values)
+			{
+				var res=0;
+				for(var i=0; i < values.length; i++)
+				{
+					res+=values[i];
+				}
+				return(res);
+			}
+			
+			part1.mapReduce(map, reduce, {out: "part1Result"}).then
+			(
+				function()
+			 {
+				console.log("J'ai fini mon mapReduce");
+				 db.close();
+			}
+			);
+		}
+	);
+});
+
+function randomIntInc (low, high) 
+{
+       return Math.floor(Math.random() * (high - low + 1) + low);
+}
+
+function generateViews()
+{
+	views = [];
+	var urls = [
+       "/",
+       "/edmond",
+       "/ksander",
+       "/basile",
+       "/ulrich",
+       "/schaal",
+       "/theo",
+   ];
+
+
+   //Generate views
+   for (var i = 0; i < 100; i++) {
+
+
+       var visite = {
+
+
+           url :  "",  //random parmi liste
+           user_id: "", //random
+           hour: "",    //0-23
+
+
+       };
+
+
+       visite.url = urls[ randomIntInc(0,urls.length-1)];
+       visite.user_id = randomIntInc(0, 10);
+       visite.hour = randomIntInc(0,23);
+
+
+       views.push(visite);
+   }
+	return views;
+}
